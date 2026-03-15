fb5c21391822adbdd4aba60f3a6e6c9dc422fe5d
Ashik Mohammed <ashikmohammed786@gmail.com>
2026-03-15 14:24:22 +0530

    Initial Commit

---
+++ ExpenseTrackerApp/ExpenseTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerAppApp.swift
+//  ExpenseTrackerApp
+//
+//  Created by Ashik Mohammed on 15/03/26.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
